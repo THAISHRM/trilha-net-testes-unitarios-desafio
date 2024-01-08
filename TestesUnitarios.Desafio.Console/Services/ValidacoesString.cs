0ec2f1e4ed48341236456eba9373fe0331127859
THAISHRM <thaishrm@hotmail.com>
2024-01-08 12:37:25 -0300

    first commit

--- TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
@@ -2,18 +2,19 @@ namespace TestesUnitarios.Desafio.Console.Services
 {
     public class ValidacoesString
     {
+        //verificar quando caracteres tem o texto
         public int RetornarQuantidadeCaracteres(string texto)
         {
             var numeroCaracteres = texto.Length;
             return numeroCaracteres;
         }
-
+        //verificar se o texto tem o caracter/texto procurado
         public bool ContemCaractere(string texto, string textoProcurado)
         {
             var contem = texto.Contains(textoProcurado);
             return contem;
         }
-
+        //se o texto termina com o texto que você está buscando
         public bool TextoTerminaCom(string texto, string textoProcurado)
         {
             var termina = texto.EndsWith(textoProcurado);
